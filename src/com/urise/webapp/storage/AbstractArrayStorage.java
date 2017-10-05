15fc388e723141b5e6f24ab4c28c61b1cd438448
Ilya_Stroev <ilya_stroev@mail.ru>
2017-10-05 19:21:05 +0300

    HW_3 hanges

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -13,6 +13,11 @@ public abstract class AbstractArrayStorage implements Storage {
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
+    public void clear(){
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
     public void update(Resume r){
         int index = getIndex(r.getUuid());
         if (index == -1) {
@@ -22,19 +27,8 @@ public abstract class AbstractArrayStorage implements Storage {
         }
     }
 
-    public void delete(String uuid){
-    }
+    public void save(Resume r) {
 
-    public int size() {
-        return size;
-    }
-    public void clear(){
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    public Resume[] getAll(){
-        return Arrays.copyOfRange(storage,0,size);
     }
 
     public Resume get(String uuid) {
@@ -44,6 +38,18 @@ public abstract class AbstractArrayStorage implements Storage {
             return null;
         }
         return storage[index];
+    }
+
+    public void delete(String uuid){
+
+    }
+
+    public Resume[] getAll(){
+        return Arrays.copyOfRange(storage,0,size);
+    }
+
+    public int size() {
+        return size;
     }
 
     protected abstract int getIndex(String uuid);
