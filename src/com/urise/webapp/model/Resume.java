7efe843409fa44d2811b1fd8627132139ffe50d6
Ilya_Stroev <ilya_stroev@mail.ru>
2017-10-11 15:35:54 +0300

    см. коммит Lesson04 Reflection and JUnit

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -1,43 +1,49 @@
 package com.urise.webapp.model;
 
+import java.util.UUID;
+
 /**
  * ru.javawebinar.basejava.model.Resume class
  */
 public class Resume implements Comparable<Resume>{
 
     // Unique identifier
-    private String uuid;
+        private final String uuid;
 
-    public String getUuid() {
-        return uuid;
+        public Resume() {
+            this(UUID.randomUUID().toString());
+        }
+
+        public Resume(String uuid) {
+            this.uuid = uuid;
+        }
+
+        public String getUuid() {
+            return uuid;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Resume resume = (Resume) o;
+
+            return uuid.equals(resume.uuid);
+        }
+
+        @Override
+        public int hashCode() {
+            return uuid.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return uuid;
+        }
+
+        @Override
+        public int compareTo(Resume o) {
+            return uuid.compareTo(o.uuid);
+        }
     }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Resume resume = (Resume) o;
-
-        return uuid.equals(resume.uuid);
-    }
-
-    @Override
-    public int hashCode() {
-        return uuid.hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return uuid;
-    }
-
-    @Override
-    public int compareTo(Resume o) {
-        return uuid.compareTo(o.uuid);
-    }
-}
---
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,10 @@
+package com.urise.webapp.storage;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by superman on 11.10.17.
+ */
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+
+}
