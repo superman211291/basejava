b5136cf557c6eba1204c895e08002a58bdd80388
Ilya_Stroev <ilya_stroev@mail.ru>
2017-10-13 15:29:28 +0300

    AbstractArrayStorageTest complete

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -2,9 +2,12 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.exeption.NotExistStorageException;
 import com.urise.webapp.model.Resume;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -13,6 +16,7 @@ import static org.junit.Assert.*;
  */
 public class AbstractArrayStorageTest {
     private Storage storage=new ArrayStorage();
+
 
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
@@ -33,31 +37,44 @@ public class AbstractArrayStorageTest {
 
     @Test
     public void clear() throws Exception {
-
+        storage.clear();
+        Assert.assertEquals(0,storage.size());
     }
 
     @Test
     public void update() throws Exception {
-
+        Resume r = new Resume(UUID_1);
+        Assert.assertFalse(storage.get(UUID_1)==r);
+        storage.update(r);
+        Assert.assertTrue(storage.get(UUID_1)==r);
     }
 
     @Test
     public void getAll() throws Exception {
-
+       Resume[] r = storage.getAll();
+       Resume[] r1 = {new Resume(UUID_1),new Resume(UUID_2), new Resume(UUID_3)};
+       Assert.assertArrayEquals(r,r1);
     }
 
     @Test
     public void save() throws Exception {
+        Resume resume = new Resume("uuid4");
+        storage.save(resume);
+        Assert.assertTrue(storage.get("uuid4").equals(resume));
 
     }
 
     @Test
     public void delete() throws Exception {
-
+        storage.delete(UUID_2);
+        Assert.assertEquals(2,storage.size());
     }
 
     @Test
     public void get() throws Exception {
+        Resume r =storage.get(UUID_1);
+        Resume r1 = new Resume(UUID_1);
+        Assert.assertEquals(r,r1);
 
     }
 
@@ -65,4 +82,5 @@ public class AbstractArrayStorageTest {
     public void getNotExist() throws Exception {
         storage.get("dummy");
     }
+
 }
